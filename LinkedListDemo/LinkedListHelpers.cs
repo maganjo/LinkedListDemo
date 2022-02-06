327396a2ed3f1a5e1737a4ee40783623dfa9de02
maganjo <kagostatic@gmail.com>
2022-02-06 07:05:42 +0300

    initial commit on Question 2 a

---
+++ LinkedListDemo/LinkedListHelpers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+	public static class LinkedListHelpers
+	{
+		public static void removeDuplicate(Node node)
+		{
+			Node node2 = null;
+			Node node1 = node;
+			while (node1 != null && node1.next != null)
+			{
+				node2 = node1;
+				int count = 0;
+				while (node2 != null && node2.next != null)
+				{
+					if (node1.data == node2.next.data)
+						count++;
+					if (count >= 2)
+					{
+						node2.next = node2.next.next;
+						count = 0;
+					}
+					else
+						node2 = node2.next;
+				}
+				node1 = node1.next;
+			}
+		}
+	}
+
+	public class Node
+	{
+		public string data;
+		public Node next;
+		public Node(string _data)
+		{
+			data = _data;
+			next = null;
+		}
+	}
+}
---
+++ LinkedListDemoTests/LinkedListHelpersTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedListDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace LinkedListDemo.Tests
+{
+	[TestClass()]
+	public class LinkedListHelpersTests
+	{
+		[TestMethod()]
+		public void removeDuplicateTest()
+		{
+			Node node = new Node("E");
+			node.next = new Node("B");
+			node.next.next = new Node("E");
+			node.next.next.next = new Node("E");
+			node.next.next.next.next = new Node("B");
+			node.next.next.next.next.next = new Node("A");
+			node.next.next.next.next.next.next = new Node("B");
+			LinkedListHelpers.removeDuplicate(node);
+			string json = new JavaScriptSerializer().Serialize(node);
+			Console.WriteLine(json);
+
+			Node testNode = new Node("E");
+			testNode.next = new Node("B");
+			testNode.next.next = new Node("E");
+			testNode.next.next.next = new Node("B");
+			testNode.next.next.next.next = new Node("A");
+
+			json = new JavaScriptSerializer().Serialize(node);
+			Console.WriteLine(json);
+			Assert.AreEqual<Node>(testNode, node);
+			//Assert.Fail();
+		}
+	}
+}
